239c602dc8741ea8aee67be87460aa01fbe335e7
Riki Fujihara <riki.fujihara@au.ey.com>
2024-02-05 11:30:30 +1100

    lifting state to app component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <h1>% Calc</h1>
+    </header>
+  );
+}
